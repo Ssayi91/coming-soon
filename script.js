1a8a7e0eb2e6d6ee1f18492e3f43bcfd77c69fd0
Sayi <sonny.sayi@gmail.com>
2025-01-18 11:42:36 +0300

    updates

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const timer = setInterval(() => {
   const now = new Date().getTime();
   const distance = launchDate - now;
 
-  const days = Math.floor(distance / (1000 * 60 * 60 * 80));
+  const days = Math.floor(distance / (1000 * 60 * 60 * 120));
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
